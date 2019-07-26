fb4c91ee93522f2bdf7d48a35ca243c23d5e34a6
HakunaMatataBert <Bert.zhang30@outlook.com>
2019-07-26 09:45:36 +0800

    添加

--- src/main/java/com/learn/springboot/controller/UserController.java
+++ src/main/java/com/learn/springboot/controller/UserController.java
@@ -1,10 +1,11 @@
 package com.learn.springboot.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-
+    @RequestMapping("/usershow")
     public String UserController(){
         return "";
     }
