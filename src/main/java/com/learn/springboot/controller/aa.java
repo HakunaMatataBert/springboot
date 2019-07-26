f6ade5a78ae4f6fb28c8cc645ab998412b686302
HakunaMatataBert <41582323+HakunaMatataBert@users.noreply.github.com>
2019-07-26 09:42:01 +0800

    Create aa.java

---
+++ src/main/java/com/learn/springboot/controller/aa.java
@@ -0,0 +1,11 @@
+package com.learn.springboot.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class aa {
+
+    public String UserController(){
+        return "";
+    }
+}
